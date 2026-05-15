9e2277c99cfb79fb2898bcc1284eb8791c023657
joshua <joshua.mbilli@student.moringaschool.com>
2026-05-15 14:35:23 +0300

    Fix GitHub social auth callback URL

--- DriveEasy/api/server.js
+++ DriveEasy/api/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: require("path").join(__dirname, "../.env") });
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
